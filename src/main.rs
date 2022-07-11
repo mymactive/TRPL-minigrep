db25cee6ef0d9778cb0842190f01a9202e2600cc
mymactive <60250695+miyazaki-masashi@users.noreply.github.com>
2022-07-12 00:03:30 +0900

    WIP:12.4

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use minigrep::{run, Config};
+use std::env;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        process::exit(1);
+    }
+}
